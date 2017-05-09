5f9dcd9b6efe66421b1ad09402594edb4896c61d
Almir Santos <almirpask@hotmail.com>
2017-05-09 11:13:13 -0300

    add homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,23 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
+  pages: Array<any> ;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    this.pages = [
+      {title:'Home', component: HomePage, icon:"home"}
+    ]
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  openPage(page:any){
+    console.log('teste');
+    this.rootPage = page.component;
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  testes: string;
   constructor(public navCtrl: NavController) {
 
   }
+
+  teste(){
+     this.testes = "vc clicou no botao teste";
+  }
+  
 
 }
